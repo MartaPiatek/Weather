a3f5896d3ebff51917ca160f72c1481fdba3fa45
MartaPiatek <martusia.piatek@gmail.com>
2017-09-12 19:24:16 +0200

    Wczytywanie przykładowych danych o temperaturze

--- app/src/main/java/pl/martapiatek/weather/MainActivity.java
+++ app/src/main/java/pl/martapiatek/weather/MainActivity.java
@@ -8,8 +8,12 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 
 public class MainActivity extends ListActivity implements Runnable {
@@ -44,9 +48,50 @@ public class MainActivity extends ListActivity implements Runnable {
 
     }
 
+    private void onDataLoaded(){
+
+        ((TextView) findViewById(R.id.currentDayOfWeek)).setText(
+                weekdays[Calendar.getInstance().get(Calendar.DAY_OF_WEEK)-1]);
+
+        ((TextView) findViewById(R.id.currentTemperature)).setText(
+                temperatureData.getCurrentConditions().get(TemperatureData.CURRENT));
+
+        ((TextView) findViewById(R.id.currentDewPoint)).setText(
+                temperatureData.getCurrentConditions().get(TemperatureData.DEW_POINT));
+
+        ((TextView) findViewById(R.id.currentHigh)).setText(
+                temperatureData.getCurrentConditions().get(TemperatureData.HIGH));
+
+        ((TextView) findViewById(R.id.currentLow)).setText(
+                temperatureData.getCurrentConditions().get(TemperatureData.LOW));
+
+        if(splashDialog != null){
+            splashDialog.dismiss();
+            splashDialog = null;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+      //  getMenuInflater().inflate(R.menu.main,menu);
+
+        return true;
+    }
 
     @Override
     public void run() {
+
+        temperatureData = new TemperatureData(this);
+        temperatureAdapter.setTemperatureData(temperatureData);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                onDataLoaded();
+            }
+        }, 5000);
+
+
 
     }
 }
